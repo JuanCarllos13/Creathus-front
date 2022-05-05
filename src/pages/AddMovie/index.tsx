315142c0df921d0ff32068f7e0c3b34cd591854e
Juan Carlos <juan.carlos@tarkuss.com.br>
2022-05-05 15:30:27 -0400

    Creating the movie registration page

--- src/pages/AddMovie/index.tsx
+++ src/pages/AddMovie/index.tsx
@@ -1,33 +1,44 @@
 import React from "react";
 import Header from "../../components/Header";
-import styles from './styles.module.scss'
-
+import styles from "./styles.module.scss";
 
 export default function AddMovie() {
-    return (
-        <div>
-            <Header />
+  return (
+    <div>
+      <Header />
 
-            <div className={styles.Content}>
+      <div className={styles.Content}>
+        <div className={styles.main}>
+          <div className={styles.areImage}></div>
 
-
-                <div className={styles.main}>
-                    <div className={styles.areImage}>
-                        hjd
-
-                    </div>
-
-                    <div className={styles.areInput}>
-                      <input type={"text"} placeholder={'Autor'} className={styles.input} />
-                      <input type={"text"} placeholder={'Autor'} className={styles.input} />
-                      <input type={"text"} placeholder={'Autor'} className={styles.input} />
-                    </div>
-
-                </div>
-
-
-
-            </div>
+          <div className={styles.areInput}>
+            <textarea
+             style={{resize: 'none'}}
+              placeholder={"Autor"}
+              className={styles.input}
+            />
+            <textarea
+              style={{resize: 'none'}}
+              placeholder={"Título "}
+              className={styles.input}
+            />
+            <textarea
+              style={{resize: 'none'}}
+              placeholder={"Descrição"}
+              className={styles.input}
+            />
+          </div>
         </div>
-    )
+
+        <div className={styles.line} />
+
+        <div className={styles.areaFile}>
+          <input type={"file"} />
+        </div>
+
+          <button className={styles.post}>Postar</button>
+          <button className={styles.cancel}>Cancelar</button>
+      </div>
+    </div>
+  );
 }
